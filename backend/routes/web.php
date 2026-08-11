023bc760eecf91981adefd3f4427c815c77f83f4
abirham <abirhammuch526@gmail.com>
2026-08-11 09:45:04 +0300

    test route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/hello', function () {
+    return 'Hello, World!';
+});
+
+Route::get('/greet/{name}', function ($name) {
+    
+    return "Hello, $name!";
+});
+
+Route::get('/search', function (Request $request) {
+    return $request->name . ' ' . $request->address;
 });
