8028f6ad1078180b998708e74c0e116c800d7243
abirham <abirhammuch526@gmail.com>
2026-09-03 07:23:55 +0300

    feat: add CORS configuration to support multiple origins

---
+++ backend/config/cors.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'paths' => ['*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => array_values(array_filter(array_map(
+        'trim',
+        explode(',', env('CORS_ALLOWED_ORIGINS', 'https://ishare-wk6j.onrender.com'))
+    ))),
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
