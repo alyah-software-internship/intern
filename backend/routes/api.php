c2d6c288b30cea438a419217fc800e20a8d3dbde
abirham <abirhammuch526@gmail.com>
2026-09-21 15:56:39 +0300

    feat: reorder product and platform currency routes for better organization

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -76,10 +76,10 @@ Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
 // Product Routes (Public)
 Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/featured', [ProductController::class, 'featured']);
-Route::get('/platform-currency', [AdminController::class, 'platformCurrency']);
 Route::get('/products/search', [ProductController::class, 'search']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::get('/platform-currency', [AdminController::class, 'platformCurrency']);
 
 // Category Routes (Public)
 Route::get('/categories', [CategoryController::class, 'index']);
